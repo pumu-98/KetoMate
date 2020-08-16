305b60144978fff827dfb4a95cb5cdd363d4f40d
Andun <andunipamoda98@gmail.com>
2020-08-16 16:14:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ketomate/MyLoginInterface.java
@@ -0,0 +1,14 @@
+package com.example.ketomate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyLoginInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_login_interface);
+    }
+}
