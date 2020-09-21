94e3ff9be042b05dbf8fef1e6fb3f7fe3895a947
vaji123 <it19126470@my.sliit.lk>
2020-09-21 11:35:04 +0530

    Update the code to insert details to the firebase

--- app/src/main/java/com/example/ketomate/MainActivity2.java
+++ app/src/main/java/com/example/ketomate/MainActivity2.java
@@ -4,11 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class MainActivity2 extends AppCompatActivity {
+    User user;
+    RadioButton medium,large;
+    FirebaseDatabase database;
+    DatabaseReference dbref;
+    CheckBox fish,chicken,egg,prawns,cuttlefish;
+    int i=0;
     Button btn;
 
     @Override
@@ -16,10 +30,89 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        user=new User();
+
+
+        medium=findViewById(R.id.radioButton2);
+        large=findViewById(R.id.radioButton3);
+        fish=findViewById(R.id.cBoxFish);
+        chicken=findViewById(R.id.cBoxChicken);
+        prawns=findViewById(R.id.cBoxPrawns);
+        egg=findViewById(R.id.cBoxEgg);
+        cuttlefish=findViewById(R.id.cBoxCuttlefish);
+
         btn=(Button)findViewById(R.id.totalPrice);
+
+        final String e="Fish";
+        final String e2="Chicken";
+        final String e3="Egg";
+        final String e4="Prawns";
+        final String e5="Cuttle fish";
+
+        dbref=database.getInstance().getReference().child("user");
+
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()){
+                    i=(int)dataSnapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String m1=medium.getText().toString();
+                String m2=large.getText().toString();
+
+                dbref.child(String.valueOf(i+1)).setValue(user);
+
+                if (medium.isChecked()){
+                    user.setOption(m1);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+                }else {
+                    user.setOption(m2);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+                }
+
+                if (fish.isChecked()){
+                    user.setAddExtra1(e);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+
+
+                }else {
+                }
+                if (chicken.isChecked()){
+                    user.setAddExtra2(e2);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+
+                }else {
+                }
+                if (egg.isChecked()){
+                    user.setAddExtra3(e3);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+
+                }else {
+                }
+                if (prawns.isChecked()){
+                    user.setAddExtra4(e4);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+
+                }else {
+                }
+                if (cuttlefish.isChecked()){
+                    user.setAddExtra5(e5);
+                    dbref.child(String.valueOf(i+1)).setValue(user);
+
+                }
 
                 openActivity2();
             }
---
+++ app/src/main/java/com/example/ketomate/User.java
@@ -0,0 +1,62 @@
+package com.example.ketomate;
+
+public class User {
+
+    private String option;
+    private String AddExtra1;
+    private String AddExtra2;
+    private String AddExtra3;
+    private String AddExtra4;
+    private String AddExtra5;
+
+    public User() {
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public String getAddExtra1() {
+        return AddExtra1;
+    }
+
+    public void setAddExtra1(String addExtra1) {
+        AddExtra1 = addExtra1;
+    }
+
+    public String getAddExtra2() {
+        return AddExtra2;
+    }
+
+    public void setAddExtra2(String addExtra2) {
+        AddExtra2 = addExtra2;
+    }
+
+    public String getAddExtra3() {
+        return AddExtra3;
+    }
+
+    public void setAddExtra3(String addExtra3) {
+        AddExtra3 = addExtra3;
+    }
+
+    public String getAddExtra4() {
+        return AddExtra4;
+    }
+
+    public void setAddExtra4(String addExtra4) {
+        AddExtra4 = addExtra4;
+    }
+
+    public String getAddExtra5() {
+        return AddExtra5;
+    }
+
+    public void setAddExtra5(String addExtra5) {
+        AddExtra5 = addExtra5;
+    }
+}
