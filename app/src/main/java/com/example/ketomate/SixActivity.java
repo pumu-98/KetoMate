47938f525bba98a25de4582e9ad84f1a031f72d6
vaji123 <it19126470@my.sliit.lk>
2020-09-23 06:35:46 +0530

    Update with the delete details in the firebase

--- app/src/main/java/com/example/ketomate/SixActivity.java
+++ app/src/main/java/com/example/ketomate/SixActivity.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SixActivity extends AppCompatActivity {
 
     Button btn1,btn2;
+    DatabaseReference dbRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +42,47 @@ public class SixActivity extends AppCompatActivity {
                 openActivity3();
             }
         });
+
+
+        btn1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DatabaseReference delRef = FirebaseDatabase.getInstance().getReference().child("user");
+                        delRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                                  @Override
+                                                                  public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                                      if(dataSnapshot.hasChild("us"))
+                                                                      {
+
+
+                                                                          dbRef = FirebaseDatabase.getInstance().getReference().child("user").child("us");
+                                                                          dbRef.removeValue();
+                                                                          // clearControls();
+
+                                                                          //Feedback to the user via a toast..
+                                                                          Toast.makeText(getApplicationContext(),"Data Deleted Sucessfully", Toast.LENGTH_SHORT).show();
+
+                                                                      }
+                                                                      else
+                                                                      {
+
+                                                                          Toast.makeText(getApplicationContext(),"No Source to Delete", Toast.LENGTH_SHORT).show();
+                                                                      }
+                                                                  }
+
+                                                                  public void onCancelled(DatabaseError databaseError)
+                                                                  {
+
+                                                                  }
+                                                              }
+                        );
+
+
+                    }
+                }
+
+        );
     }
 
     public void openActivity2() {
